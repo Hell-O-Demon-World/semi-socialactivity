0c684464ac7f2184881d2ed7bb662d7040984ff4
darkblose <darkblose@gmail.com>
2022-09-27 11:27:02 +0900

    feature : QuitActivityController Create

--- src/main/java/com/golfzone/social/controller/activity/QuitActivityController.java
+++ src/main/java/com/golfzone/social/controller/activity/QuitActivityController.java
@@ -10,34 +10,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JoinActivityController extends HttpServlet {
+public class QuitActivityController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         String activityMemberResultMsg = "";
         // init get parameter
+        String activityMemberNum = req.getParameter("activityMemberNum");
         String activityNum = req.getParameter("activityNum");
         String userNum = req.getParameter("userNum");
         String clubNum = req.getParameter("clubNum");
         ActivityMemberVO activityMemberVO = new ActivityMemberVO();
         ActivityMemberDAO activityMemberDAO = new ActivityMemberDAOImpl();
 
+        activityMemberVO.setActivityMemberNum(Integer.parseInt(activityMemberNum));
         activityMemberVO.setActivityNum(Integer.parseInt(activityNum));
         activityMemberVO.setUserNum(Integer.parseInt(userNum));
         activityMemberVO.setClubNum(Integer.parseInt(clubNum));
 
         // Checking Number, input nothing
-        if (activityMemberDAO.findUserByActivityNum(activityMemberVO).getActivityNum() != 0) {
-            activityMemberResultMsg = "이미가입된 액티비티야 ";
+        if (activityMemberDAO.findUserByActivityNum(activityMemberVO).getActivityNum() == 0) {
+            activityMemberResultMsg = "가입하지 않은 액티비티입니다.";
         } else {
             // init VO, DAO
             activityMemberDAO = new ActivityMemberDAOImpl();
             activityMemberVO = new ActivityMemberVO();
+            activityMemberVO.setActivityMemberNum(Integer.parseInt(activityMemberNum));
             activityMemberVO.setActivityNum(Integer.parseInt(activityNum));
             activityMemberVO.setClubNum(Integer.parseInt(clubNum));
             activityMemberVO.setUserNum(Integer.parseInt(userNum));
-            activityMemberDAO.insertActivityMember(activityMemberVO);
-            activityMemberResultMsg = "가입완료..";
+            activityMemberDAO.deleteActivityMember(activityMemberVO);
+            activityMemberResultMsg = "탈퇴완료...";
         }
         System.out.println(activityMemberResultMsg);
 
