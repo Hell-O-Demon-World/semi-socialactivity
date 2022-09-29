3bd8a4ae61243de33027a25d0b10e06c091612bb
LEECHANHYUNG <lsj721538@gmail.com>
2022-09-29 10:06:56 +0900

    update swiper slide

--- src/main/webapp/js/swiper.js
+++ src/main/webapp/js/swiper.js
@@ -1,8 +1,18 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".recommendSwiper", {
+    slidesPerView: 2, // 한 번에 보여질 element의 개수
+    spaceBetween: 30, // 슬라이드 간격(px)
+    slidesPerGroup: 2,
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    loopFillGroupWithBlank: true, // 빈칸인 경우 빈칸으로 메우기
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
     observer: true,
     observeParents: true,
 });
