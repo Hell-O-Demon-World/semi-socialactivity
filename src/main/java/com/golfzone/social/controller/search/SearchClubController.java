f653c053882662c8271555331c531128a6dcdbb5
darkblose <darkblose@gmail.com>
2022-10-03 16:10:40 +0900

    fix : search

--- src/main/java/com/golfzone/social/controller/search/SearchClubController.java
+++ src/main/java/com/golfzone/social/controller/search/SearchClubController.java
@@ -1,5 +1,10 @@
 package com.golfzone.social.controller.search;
 
+import com.golfzone.social.club.ClubVO;
+import com.golfzone.social.search.SearchClubDAO;
+import com.golfzone.social.search.SearchClubDAOImpl;
+import com.golfzone.social.search.SearchClubVO;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -69,8 +74,16 @@ public class SearchClubController extends HttpServlet {
             }
         }
         req.setAttribute("userNum", userNum);
+        req.setAttribute("searchClubVO", searchClubVO);
+
+        req.setAttribute("searchLocation", searchLocation);
+        req.setAttribute("searchTitle", searchTitle);
+        req.setAttribute("searchMinAge", searchMinAge);
+        req.setAttribute("searchMaxAge", searchMaxAge);
+        req.setAttribute("searchMinScore", searchMinScore);
+        req.setAttribute("searchMaxScore", searchMaxScore);
+
         req.setAttribute("resultMsg", clubSearchResultMsg);
         req.getRequestDispatcher("/search/search.jsp").forward(req, resp);
-
     }
 }
